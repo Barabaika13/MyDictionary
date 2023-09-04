82877b9bbd41175fb198424a8f90f8d33f22170d
ZaDDrot PC <ZaDDrot PC@COMPUTER>
2023-09-04 18:14:05 +0300

    Added indexer

--- MyDictionary/Program.cs
+++ MyDictionary/Program.cs
@@ -10,12 +10,19 @@
             myDict.Add(31, "thirty one");
             myDict.Add(32, "thirty two");
             myDict.Add(0, "zero");
-            myDict.Add(33, "thirty three");
-            myDict.Add(34, "thirty four");
-            myDict.Add(63, "sixty three");
-            myDict.Add(64, "sixty four");
-            myDict.Add(15, null);
-            myDict.Add(-23, "minus twenty three");
+            //myDict.Add(33, "thirty three");
+            //myDict.Add(34, "thirty four");
+            //myDict.Add(63, "sixty three");
+            //myDict.Add(64, "sixty four");            
+            //myDict.Add(15, null);
+            //myDict.Add(-23, "minus twenty three");
+
+            myDict[35] = "Hello";
+            string value1 = myDict[35];
+            Console.WriteLine(value1);
+            myDict[65] = "Bye";
+            string value2 = myDict[65];
+
 
             myDict.Get(31);
             myDict.Get(10);
@@ -44,8 +51,8 @@
 
         private class KVPair
         {
-            public int Key { get; set; }
-            public string Value { get; set; }
+            public int Key { get; }
+            public string Value { get; }
 
             public KVPair(int key, string value)
             {
@@ -54,38 +61,61 @@
             }
         }
 
+        public string this[int key]
+        {
+            get
+            {
+                return Get(key);
+            }
+            set
+            {
+                Add(key, value);
+            }
+        }
+
         public void Add(int key, string value)
         {
-            try
+
+            if (value == null)
             {
-                if (value == null)
+                Console.WriteLine("Словарь не принимает пустую строку в качестве значения");
+                return;
+            }
+            else
+            {
+                int index = Math.Abs(key % _myArray.Length);
+                //int index = CalculateIndex(key);
+                if (_myArray[index] == null)
                 {
-                    throw new ArgumentNullException(nameof(value));
+                    _myArray[index] = new KVPair(key, value);
                 }
                 else
                 {
-                    int index = Math.Abs(key % _myArray.Length);
-                    if (_myArray[index] == null)
-                    {
-                        _myArray[index] = new KVPair(key, value);
-                    }
-                    else
-                    {
-                        CreateBiggerArray(key, value);
-                    }
-
-                    //if (_myArray[index] != null)
-                    //{
-                    //    var e = new Exception("Место занято");
-                    //    throw e;
-                    //}
+                    CreateBiggerArray(key, value);
                 }
+
+                //if (_myArray[index] != null)
+                //{
+                //    var e = new Exception("Место занято");
+                //    throw e;
+                //}
             }
-            catch (ArgumentNullException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+
+            //try
+            //{
+
+            //}
+            //catch (ArgumentNullException e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}               
         }
+
+        //private int CalculateIndex(int key)
+        //{
+        //    return Math.Abs(key) % _myArray.Length;
+        //}
+
 
         public void CreateBiggerArray(int key, string value)
         {
@@ -97,6 +127,7 @@
                 if (oldArray[i] != null)
                 {
                     int newIndexOfOldValue = Math.Abs(oldArray[i].Key % _myArray.Length);
+                    //int newIndexOfOldValue = CalculateIndex(key);
                     _myArray[newIndexOfOldValue] = oldArray[i];
                 }
             }
@@ -107,6 +138,7 @@
         public string Get(int key)
         {
             int index = Math.Abs(key % _myArray.Length);
+            //int index = CalculateIndex(key);
             if (_myArray[index] == null)
             {
                 Console.WriteLine($"Element with key {key} not found");
